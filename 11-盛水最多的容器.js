c9c441f7c8eae83a6d69904086c593ac524cf2e8
xudongz <zxd@gato.com.cn>
2020-09-28 16:00:02 +0800

    第八题 取巧的方法 但是也在里面留下了 状态机的代码  需要研究研究 11题 很简单 双指针 头尾向中间聚集  谁高度小 就移动谁

---
+++ 11-盛水最多的容器.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function (height) {
+	if (height.length<2) return 0;
+	let left = 0, right = height.length;
+	let max = 0;
+	let getMax = () => Math.max(max, (right - left) *  Math.min(~~height[left], ~~height[right]));
+	while(left< right) {
+		if (~~height[left] < ~~height[right]) {
+			max = getMax();
+			left++;
+		} else {
+			max = getMax();
+			right--;
+		}
+	}
+	return max;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([8, 8, 8]));
