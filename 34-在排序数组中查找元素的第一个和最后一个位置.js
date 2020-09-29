f5fa0bc733a9dde093148abdfac12dd6e7a6e153
xudongz <zxd@gato.com.cn>
2020-09-29 17:41:30 +0800

    二分法 坑在于 找到符合的元素以后 要向两边推进 直到最后一个为止

--- 34-在排序数组中查找元素的第一个和最后一个位置.js
+++ 34-在排序数组中查找元素的第一个和最后一个位置.js
@@ -3,6 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
-const searchRange = function(nums, target) {
-
+const searchRange = function (nums, target) {
+	let res = [-1, -1];
+	if (nums.length === 1) return target === nums[0] ? [0, 0] : res;
+	let start = 0, end = nums.length - 1, mid, left = 0, right = 0;
+	while (start <= end) {
+		mid = start + ((end - start) >> 1);
+		if (nums[mid] === target) {
+			left = mid;
+			right = mid;
+			while (left > start && nums[left] === nums[left - 1]) left--;
+			while (right < end && nums[right] === nums[right + 1]) right++;
+			return [left, right]
+		}
+		if (nums[mid] > target) {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	return res;
 };
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6));
+console.log(searchRange([7, 7], 7));
+console.log(searchRange([7], 7));
+console.log(searchRange([1, 3], 1));
+console.log(searchRange([3, 3, 3], 3));
